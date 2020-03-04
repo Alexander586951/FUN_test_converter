69af856e28d5c4fdcc6bec01030fb8c00a7fd289
Alexander Vinogradov <dra-katt@yandex.ru>
2020-03-04 18:06:45 +0300

    4.03.2020: добавлена обработка вопросов на последовательность и на сопоставление

--- converter_app/test_file_converter.py
+++ converter_app/test_file_converter.py
@@ -6,8 +6,6 @@ from datetime import date
 from pathlib import Path
 
 
-# TODO: def a function with regex match for input and list of strings for output
-
 def nonblank_lines(f):
     """
     from https://stackoverflow.com/questions/4842057/easiest-way-to-ignore-blank-lines-when-reading-a-file-in-python
@@ -26,56 +24,87 @@ def randomize_list(source_list):
     return source_list
 
 
-def find_a_match(regex, source):
-    pass
+def isfloat(value):
+    """
+    from https://stackoverflow.com/a/20929881
+    :param value: string that could be converted to float
+    :return: Boolean
+    """
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
 
 
 def process_file(act_input_file, gift_output_file):
     test_head_data = {}
     test_data = {}
 
+    left_question_pattern = r'L[0-9]?\:'
+    right_question_pattern = r'R[0-9]?\:'
+    sequence_pattern = r'[0-9]?\:'
+
     with open(act_input_file, encoding='utf-8') as f_i:
-        input_filename = Path(act_input_file).parts[-1]
         i_counter = 0
         for line in nonblank_lines(f_i):
 
             if 'V1:' in line:
-                line = line.replace('V1: ', '')
+                line = line.replace('V1: ', '').replace('V1:', '')
                 test_head_data['head_1'] = line.strip()
             elif 'V2:' in line:
-                line = line.replace('V2: ', '')
+                line = line.replace('V2: ', '').replace('V2:', '')
                 test_head_data['head_2'] = line.strip()
             elif 'I:' in line:
                 i_counter += 1
                 test_data[f'{i_counter}'] = {}
                 test_data[f'{i_counter}']['head'] = \
-                    line.replace('I:', '').strip()
+                    line.replace('I: ', '').replace('I:', '').strip()
                 test_data[f'{i_counter}']['pos_answer'] = []
                 test_data[f'{i_counter}']['neg_answer'] = []
-                test_data[f'{i_counter}']['left_column'] = []
-                test_data[f'{i_counter}']['right_column'] = []
+                test_data[f'{i_counter}']['left_answer'] = []
+                test_data[f'{i_counter}']['right_answer'] = []
             elif 'S:' in line:
                 test_data[f'{i_counter}']['body'] = \
-                    line.replace('S:', '').replace('### ###', '').strip()
+                    line.replace('S: ', '').replace('S:', '')\
+                        .replace('#', '').strip()
+
+            # обработка вопроса на последовательность
+            elif re.match(left_question_pattern, line):
+                pattern = re.compile(left_question_pattern)
+                left_line = pattern.sub('', line)
+                test_data[f'{i_counter}']['left_answer']\
+                    .append(left_line.strip())
+            elif re.match(right_question_pattern, line):
+                pattern = re.compile(right_question_pattern)
+                right_line = pattern.sub('', line)
+                test_data[f'{i_counter}']['right_answer']\
+                    .append(right_line.strip())
+
+            # преобразуем вопрос на последовательность
+            # в вопрос на сопоставление
+            elif re.match(sequence_pattern, line):
+                seq_line = line.split(':')
+                test_data[f'{i_counter}']['left_answer']\
+                    .append(seq_line[0])
+                test_data[f'{i_counter}']['right_answer']\
+                    .append(seq_line[1])
+
             elif '+:' not in line and '-:' not in line:
                 test_data[f'{i_counter}']['body'] += ('\n' + line)
             elif '+:' in line:
                 test_data[f'{i_counter}']['pos_answer'] \
-                    .append(line.replace('+:', '').strip())
+                    .append(line.replace('+: ', '').replace('+:', '')
+                            .strip())
             elif '-:' in line:
                 test_data[f'{i_counter}']['neg_answer'] \
-                    .append(line.replace('-:', '').strip())
-    # test prints
-    # for k in test_head_data:
-    #     print('test_head:', k, ':', test_head_data[k])
-    # for k in test_data:
-    #     print('test_data:', k, ':', test_data)
+                    .append(line.replace('-: ', '').replace('-:', '')
+                            .strip())
+
+    # print(test_data)
 
     with open(gift_output_file, 'w', encoding='utf-8') as o_f:
-        # line stamp to confirm successful file exchange
-        o_f.write(f"//source: {input_filename}, conversion date: {date.today()}\n")
-        o_f.write("//Processed through Django web application")
-        o_f.write(' \n')
 
         o_f.write(f"//H1: {test_head_data['head_1']}\n\n")
         o_f.write(f"//H2: {test_head_data['head_2']}\n")
@@ -88,6 +117,15 @@ def process_file(act_input_file, gift_output_file):
                       f"name: {test_data[q]['head']}\n")
 
             neg_answer_count = len(test_data[q]['neg_answer'])
+            l_answer_count = len(test_data[q]['left_answer'])
+            r_answer_count = len(test_data[q]['right_answer'])
+
+            # GIFT не поддерживает вопросы на сопоставление с неравными
+            # списками вариантов. Искусственно выравниваем их.
+            while len(test_data[q]['right_answer']) < len(test_data[q]['left_answer']):
+                test_data[q]['right_answer'].append(' ')
+            while len(test_data[q]['left_answer']) < len(test_data[q]['right_answer']):
+                test_data[q]['left_answer'].append(' ')
 
             # обработка вопросов multiple choice
             if neg_answer_count >= 1:
@@ -109,15 +147,38 @@ def process_file(act_input_file, gift_output_file):
                 o_f.write(" \n")
 
             # обработка вопросов fill in answer
-            elif neg_answer_count == 0:
+            elif neg_answer_count == 0 and l_answer_count == 0:
                 o_f.write(f"::{test_data[q]['head']}::")
-                q_body = test_data[q]['body'].split("…")
-                o_f.write(f"{q_body[0]} "
-                          + "{=" + f"{test_data[q]['pos_answer'][0]}" + "}"
-                          + f" {q_body[1]}\n")
+                if '...' in line or '…' in line:
+                    q_body = test_data[q]['body'].replace('...', '…')\
+                        .replace('#', '').split("…")
+                    o_f.write(f"{q_body[0]} "
+                              + "{=" + f"{test_data[q]['pos_answer'][0]}" + "}"
+                              + f" {q_body[1]}\n")
+                else:
+                    o_f.write(f"{test_data[q]['body']} " + "{")
+                    for pos_answer in test_data[q]['pos_answer']:
+                        if isfloat(pos_answer): # TODO: заменить на корректное выстраивание множественных цифровых ответов
+                            print("it's digits!")
+                            o_f.write(f"#{pos_answer} ")
+                        else:
+                            o_f.write(f"={pos_answer} ")
+                    o_f.write('}\n')
                 o_f.write(' \n')
 
-    # return o_f
+            # обработка вопросов на сопоставление
+            elif neg_answer_count == 0 and l_answer_count > 0:
+                left_answers = test_data[q]['left_answer']
+                right_answers = test_data[q]['right_answer']
+
+                match_list = list(zip(left_answers,right_answers))
+
+                o_f.write(f"::{test_data[q]['head']}::{test_data[q]['body']} ")
+                o_f.write("{\n")
+                for pair in match_list:
+                    o_f.write(f"\t={pair[0]}\t-> {pair[1]} \n")
+                o_f.write('}\n')
+                o_f.write(' \n')
 
 
 def convert():
